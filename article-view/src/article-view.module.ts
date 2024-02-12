16cf0213114dfd785da2f41e128052cc7a977268
Sunil Kumar <sunil.divyam@gmail.com>
2024-02-12 19:38:15 +0530

    Articles UI enhancements

--- article-view/src/article-view.module.ts
+++ article-view/src/article-view.module.ts
@@ -1,21 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ArticleModule } from '@annuadvent/ngx-cms/article';
-import { ArticleViewComponent } from './components/article-view/article-view.component';
-
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ArticleModule } from "@annuadvent/ngx-cms/article";
+import { ArticleViewComponent } from "./components/article-view/article-view.component";
 
 @NgModule({
-  declarations: [
-    ArticleViewComponent
-  ],
-  imports: [
-    CommonModule,
-    ArticleModule,
-  ],
-  exports: [
-    ArticleViewComponent
-  ],
+  declarations: [ArticleViewComponent],
+  imports: [CommonModule, ArticleModule],
+  exports: [ArticleViewComponent],
 })
-export class ArticleViewModule { }
+export class ArticleViewModule {}
--- article-view/src/components/article-view/article-view.component.ts
+++ article-view/src/components/article-view/article-view.component.ts
@@ -1,22 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { UtilsService } from '@annuadvent/ngx-core/utils';
-import { Article } from '@annuadvent/ngx-cms/article';
+import { Component, Input, OnInit } from "@angular/core";
+import { UtilsService } from "@annuadvent/ngx-core/utils";
+import { Article } from "@annuadvent/ngx-cms/article";
 
 @Component({
-  selector: 'anu-article-view',
-  templateUrl: './article-view.component.html',
-  styleUrls: ['./article-view.component.scss']
+  selector: "anu-article-view",
+  templateUrl: "./article-view.component.html",
+  styleUrls: ["./article-view.component.scss"],
 })
 export class ArticleViewComponent implements OnInit {
   @Input() value: Article | null = null;
   @Input() showMetaInfo: boolean = false;
   @Input() showAside: boolean = false;
-  @Input() leftAsideClasses: Array<string> = ['col-sm-12', 'col-md-6', 'col-9'];
-  @Input() rightAsideClasses: Array<string> = ['col-sm-12', 'col-md-6', 'col-3'];
+  @Input() leftAsideClasses: Array<string> = ["col-sm-12", "col-md-6", "col-9"];
+  @Input() rightAsideClasses: Array<string> = [
+    "col-sm-12",
+    "col-md-6",
+    "col-3",
+  ];
 
-  constructor(public utilsSvc: UtilsService) { }
+  constructor(public utilsSvc: UtilsService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
